75fe47ca52be29f5348e8af34fbcfb888da112df
kikeestradadev <125921116+kikeestradadev@users.noreply.github.com>
2026-06-26 17:37:21 -0600

    ajustes

--- src/js/modules/mediaQueriesJs.js
+++
@@ -1,9 +0,0 @@
-const mediumBp = matchMedia('(min-width: 600px)');
-const changeSize = mql => {
-  mql.matches 
-    ? document.body.style.background = 'red'
-    : document.body.style.background = 'yellow'
-}
-
-mediumBp.addListener(changeSize);
-changeSize(mediumBp);
