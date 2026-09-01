035526d7515e03cec29c86c920fa31b26c3d54d7
kikeestradadev <125921116+kikeestradadev@users.noreply.github.com>
2026-08-31 23:06:18 -0600

    clean

--- src/js/modules/coreModule.js
+++
@@ -1,8 +0,0 @@
-const coreModule = () => {
-	document.querySelectorAll('.core-module').forEach((root) => {
-		// Toda consulta y listener queda acotado a root.
-		root.dataset.coreModuleReady = 'true';
-	});
-};
-
-export default coreModule;
--- src/js/modules/internalModule.js
+++
@@ -1,8 +0,0 @@
-const internalModule = () => {
-	document.querySelectorAll('.internal-module').forEach((root) => {
-		// Toda consulta y listener queda acotado a root.
-		root.dataset.internalModuleReady = 'true';
-	});
-};
-
-export default internalModule;
